9398e6aed7be083c7179cb096f18df67976b254a
greenhas <spencer.greenhalgh@posteo.net>
2022-05-09 21:02:10 -0400

    update script

--- blog_repopulation.R
+++ blog_repopulation.R
@@ -2,65 +2,28 @@ library(tidyverse)
 
 file_list <- list.files("/Users/spencergreenhalgh/hugo-testing/content/Communities", pattern = ".md", full.names = TRUE)
 
-file_list[2]
-
-temp_file <- read_lines(file_list[2])
-
-temp_file
-
-temp_file[4] <- "tags: [\"micro\",\"Communities\"]"
-
-write_lines(temp_file,paste0("/Users/spencergreenhalgh/hugo-testing/content/Communities/",write_name))
-
-
 for(i in 2:length(file_list)){
   
   temp_file <- read_lines(file_list[i])
   
-  if(temp_file[7] == "categories = [\"macro\"]"){
+  
+  if(temp_file[4] == "tags: [\"micro\"]"){
+    
+    temp_file[6] <- temp_file[3] %>% str_replace("title: \"","") %>% str_replace("title: \"","\"")    
+    temp_file[3] <- "title: \"\""
+    temp_file[4] <- "tags: [\"micro\",\"Communities\"]"
+    
+    write_lines(temp_file, file_list[i])
+    # write_lines(temp_file,paste0("/Users/spencergreenhalgh/hugo-testing/content/",file_list[i]))
     next
   }
   
-  if(temp_file[7] == "categories = [\"micro\"]"){
-    temp_file[c(1,8)] <- "---"
+  if(temp_file[4] == "tags: [\"link\"]"){
     
-    temp_file[3] <- temp_file[3] %>% str_replace(" =",":")
-    temp_file[4] <- temp_file[4] %>% str_replace(" =",":")
-    
-    write_name <- temp_file[5] %>% 
-      str_replace("slug = \"", "") %>% 
-      str_replace("\"","") %>%
-      str_replace_all("[!',()]","") %>%
-      str_c(".md")
-    
-    temp_file[7] <- temp_file[7] %>% str_replace("categories =","tags:")
-    
-    temp_file <- temp_file[c(1,3,4,7,8)]
-    
-    temp_file
-    
-    write_lines(temp_file,paste0("/Users/spencergreenhalgh/hugo-testing/content/",write_name))
-    next
-  }
-  
-  if(temp_file[7] == "categories = [\"link\"]"){
-    temp_file[c(1,8)] <- "---"
-    
-    temp_file[3] <- temp_file[3] %>% str_replace(" =",":")
-    temp_file[4] <- temp_file[4] %>% str_replace(" =",":")
-    
-    write_name <- temp_file[5] %>% 
-      str_replace("slug = \"", "") %>% 
-      str_replace("\"","") %>%
-      str_replace_all("[!',()]","") %>%
-      str_c(".md")
-    
-    temp_file[7] <- temp_file[7] %>% str_replace("categories =","tags:")
-    
-    temp_file <- temp_file[c(1,3,4,7,8,9,10,11)]
-    
-    write_lines(temp_file,paste0("/Users/spencergreenhalgh/hugo-testing/content/",write_name))
+    temp_file[4] <- "tags: [\"micro\",\"Communities\"]"
+    write_lines(temp_file, file_list[i])
+    # write_lines(temp_file,paste0("/Users/spencergreenhalgh/hugo-testing/content/",file_list[i]))
   }
   print(i)
-
+  
 }
