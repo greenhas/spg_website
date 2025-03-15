82ed44c75593f2e0609422eea781a073fe6142aa
Spencer Greenhalgh <greenhas@users.noreply.github.com>
2025-03-15 16:17:30 -0400

    hail mary

--- content/Communities/2023-11-07-Yesssss!-Andy.md
+++ content/Communities/2023-11-07-Yesssss!-Andy.md
@@ -3,7 +3,7 @@ types: ["link"]
 date: 2023-11-07T21:04:38-05:00
 layout: link
 title: "🔗 linkblog: Democrat Andy Beshear defeats Trump-endorsed Daniel Cameron for Kentucky governor "
-art_title: "Democrat Andy Beshear defeats Trump-endorsed Daniel Cameron for Kentucky governor ""
+art_title: "Democrat Andy Beshear defeats Trump-endorsed Daniel Cameron for Kentucky governor"
 art_link: "https://www.kentucky.com/article281246543.html"
 source: ["kentucky.com"]
 tags: ["2023 Kentucky elections","Andy Beshear"]
--- linkblog_updates.R
+++ linkblog_updates.R
@@ -1,81 +1,85 @@
 library(tidyverse)
 
-file_list <- list.files("/Users/spencergreenhalgh/spg_website/content/Myself", pattern = ".md", full.names = TRUE)
+file_list <- list.files("/Users/spencergreenhalgh/spg_website/content/Communities", pattern = ".md", full.names = TRUE)
 
 file_list[300]
 
 
-for(i in 1:length(file_list)){ # 1:length(file_list)
+for(i in 300:300){ # 1:length(file_list)
   
   temp_file <- read_lines(file_list[i])
+  
+  temp_file
   
   
   if(grepl("link",temp_file[2])){
     
-    temp_link <- temp_file[length(temp_file)] %>% str_remove("\\[.*\\(") %>% str_remove("\\)$")
+    print(temp_file[6])
     
-    temp_source <- temp_link %>% str_remove("https://") %>% str_remove("/.*$") %>% str_remove("www\\.")
-    
-    if(grepl("title:",temp_file[4])){
-      
-      temp_title <<- temp_file[4] %>% 
-        str_remove("just read '") %>%
-        str_remove("my thoughts on '") %>%
-        str_remove("mes pensées sur '")
-        
-      temp_art_title <- temp_title %>%
-        str_remove("title: \"🔗 linkblog: ") %>%
-        str_remove("'\"")
-      
-    }
-    if(grepl("title:",temp_file[4])){
-      
-      temp_title <<- temp_file[4] %>% 
-        str_remove("my thoughts on '") %>%
-        str_remove("mes pensées sur '")
-        
-      temp_art_title <- temp_title %>%
-        str_remove("title: \"🔗 linkblog: ") %>%
-        str_remove("'\"")
-      
-    }
-    
-    if(grepl("tags:",temp_file[4])){
-      
-      temp_tags <<- temp_file[4]
-      
-    }
-    
-    if(grepl("title:",temp_file[5])){
-      
-      temp_title <<- temp_file[5] %>% 
-        str_remove("my thoughts on '") %>%
-        str_remove("mes pensées sur '")
-      
-      temp_art_title <- temp_title %>%
-        str_remove("title: \"🔗 linkblog: ") %>%
-        str_remove("'\"")
-      
-    }
-    
-    if(grepl("tags:",temp_file[5])){
-      
-      temp_tags <<- temp_file[5]
-      
-    }
-    
-   
-    
-    temp_file <- c(
-      temp_file[1:3],
-      "layout: link",
-      temp_title,
-      paste0("art_title: \"",temp_art_title,"\""),
-      paste0("art_link: \"",temp_link,"\""),
-      paste0("source: [\"",temp_source,"\"]"),
-      temp_tags,
-      temp_file[6:(length(temp_file)-2)]
-    )
+    # temp_link <- temp_file[length(temp_file)] %>% str_remove("\\[.*\\(") %>% str_remove("\\)$")
+    # 
+    # temp_source <- temp_link %>% str_remove("https://") %>% str_remove("/.*$") %>% str_remove("www\\.")
+    # 
+    # if(grepl("title:",temp_file[4])){
+    #   
+    #   temp_title <<- temp_file[4] %>% 
+    #     str_remove("just read '") %>%
+    #     str_remove("my thoughts on '") %>%
+    #     str_remove("mes pensées sur '")
+    #     
+    #   temp_art_title <- temp_title %>%
+    #     str_remove("title: \"🔗 linkblog: ") %>%
+    #     str_remove("'\"")
+    #   
+    # }
+    # if(grepl("title:",temp_file[4])){
+    #   
+    #   temp_title <<- temp_file[4] %>% 
+    #     str_remove("my thoughts on '") %>%
+    #     str_remove("mes pensées sur '")
+    #     
+    #   temp_art_title <- temp_title %>%
+    #     str_remove("title: \"🔗 linkblog: ") %>%
+    #     str_remove("'\"")
+    #   
+    # }
+    # 
+    # if(grepl("tags:",temp_file[4])){
+    #   
+    #   temp_tags <<- temp_file[4]
+    #   
+    # }
+    # 
+    # if(grepl("title:",temp_file[5])){
+    #   
+    #   temp_title <<- temp_file[5] %>% 
+    #     str_remove("my thoughts on '") %>%
+    #     str_remove("mes pensées sur '")
+    #   
+    #   temp_art_title <- temp_title %>%
+    #     str_remove("title: \"🔗 linkblog: ") %>%
+    #     str_remove("'\"")
+    #   
+    # }
+    # 
+    # if(grepl("tags:",temp_file[5])){
+    #   
+    #   temp_tags <<- temp_file[5]
+    #   
+    # }
+    # 
+    # 
+    # 
+    # temp_file <- c(
+    #   temp_file[1:3],
+    #   "layout: link",
+    #   temp_title,
+    #   paste0("art_title: \"",temp_art_title,"\""),
+    #   paste0("art_link: \"",temp_link,"\""),
+    #   paste0("source: [\"",temp_source,"\"]"),
+    #   temp_tags,
+    #   temp_file[6:(length(temp_file)-2)]
+    # )
     
     # print(temp_file)
 
